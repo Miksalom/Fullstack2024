83cdb2adad5a51ecded64e9077e3a0f153a544a5
Mikael Palojärvi <miksu.palojarvi@gmail.com>
2024-02-04 21:11:23 +0200

    tehtävät 1.3-1.5 tehty

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,39 +1,53 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const courses = [
-    {name: 'Fundamentals of React', lkm: 10},
-    {name: 'Using props to pass data', lkm: 7},
-    {name: 'State of a component', lkm: 14},
-  ]
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+  {
+    name: 'Fundamentals of React',
+    lkm: 10
+  },
+  {
+    name: 'Using props to pass data',
+    lkm: 7
+  },
+  {
+    name: 'Using props to pass data',
+    lkm: 14
+  }
+]
+}
+  
   return (
     <div>
       <Header course={course}/>
-      <Content osat={courses} />
-      <Total osat={courses} />
+      <Content osat={course} />
+      <Total osat={course} />
 
     </div>
   )
 }
 const Header = (props) =>{
+  console.log(props)
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </div>
   )
 }
 
 const Content = (props) =>{
+  console.log(props)
   return (
     <div>
-      <Part name={props.osat[0].name} lkm={props.osat[0].lkm}/>
-      <Part name={props.osat[1].name} lkm={props.osat[1].lkm}/>
-      <Part name={props.osat[2].name} lkm={props.osat[2].lkm}/>
+      <Part name={props.osat.parts[0].name} lkm={props.osat.parts[0].lkm}/>
+      <Part name={props.osat.parts[1].name} lkm={props.osat.parts[1].lkm}/>
+      <Part name={props.osat.parts[2].name} lkm={props.osat.parts[2].lkm}/>
     </div>
   )
 }
 
 const Part = (props) =>{
+  console.log(props)
   return(
     <div>
       <p>{props.name} {props.lkm} </p>
@@ -42,9 +56,10 @@ const Part = (props) =>{
 }
 
 const Total = (props) =>{
+  console.log(props)
   return(
     <div>
-      <p>Number of exercises {props.osat[0].lkm + props.osat[1].lkm + props.osat[2].lkm}</p>
+      <p>Number of exercises {props.osat.parts[0].lkm + props.osat.parts[1].lkm + props.osat.parts[2].lkm}</p>
     </div>
   )
 }
