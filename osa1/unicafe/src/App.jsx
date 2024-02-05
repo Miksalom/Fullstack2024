8998df664f625017d56a986154a07445fc8509ff
Mikael Palojärvi <miksu.palojarvi@gmail.com>
2024-02-05 18:56:29 +0200

    Tehty 1.6-1.11 tehtävät unicafe

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,35 +1,80 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+const Statistics = (props) => {
 
-  return (
-    <>
+  const sum = props.good + props.neutral + props.bad
+  const average = (props.good - props.bad) / sum
+  const positive = (props.good/sum)*100 + " %"
+  if (sum === 0){
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Statistics</h2>
+        No feedback given
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    )
+  }
+  return(
+    <div>
+      <h2>Statistics</h2>
+      <StatisticLine text='good' value={props.good} />
+      <StatisticLine text='neutral' value={props.neutral} />
+      <StatisticLine text='bad' value={props.bad} />
+      <StatisticLine text='all' value={sum} />
+      <StatisticLine text='average' value={average} />
+      <StatisticLine text='positive' value={positive} />
+    </div>
   )
 }
+const StatisticLine = ({text, value}) => {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>{text}</td>
+          <td>{value}</td>
+        </tr>
+      </tbody>
+    </table>
+     
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increseGood = () =>{
+    return(
+      setGood(good + 1)
+    )
+  }
+  const increseNeutral = () =>{
+    return(
+      setNeutral(neutral + 1)
+    )
+  }
+  const increseBad = () =>{
+    return(
+      setBad(bad + 1)
+    )
+  }
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={increseGood} text='Good'/>
+      <Button handleClick={increseNeutral} text='Neutral'/>
+      <Button handleClick={increseBad} text='Bad'/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
 
 export default App
