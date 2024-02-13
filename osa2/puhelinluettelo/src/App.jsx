7c446c3ae892649e5ef28f929e902516b83bcbcb
Mikael Palojärvi <miksu.palojarvi@gmail.com>
2024-02-13 21:19:24 +0200

    2.8 tehtävä tehty

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,36 +2,53 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
+      nummber: newNumber,
     }
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
     } else {
+      console.log(personObject)
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
     
     
   }
 
-  const handlePersonChange = (event) => {
+  const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+
+  }
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <form>
+        <div>filter shown with <input/> </div>
+      </form>
+      <h1>add a new</h1>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handlePersonChange}/>
+          <div>name: <input value={newName} onChange={handleNameChange}/></div>
+          <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         </div>
         <div>
           <button type="submit">add</button>
@@ -39,7 +56,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <b>
-        {persons.map(person => (<p key={person.name} >{person.name} </p>))}
+        {persons.map(person => (
+        <p key={person.name}>{person.name} {person.number} </p>
+        ))}
       </b>
     </div>
   )
